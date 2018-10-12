fe55d917a5d7506370d00a94b51e570890dbdece
Chingis Radnaev <chingis.radnaew@mail.ru>
2018-10-12 22:57:04 +0700

    initial. Generated Wsdl domain classes

---
+++ src/main/java/com/radn/wsdl_api/WsdlApiApplication.java
@@ -0,0 +1,12 @@
+package com.radn.wsdl_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsdlApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsdlApiApplication.class, args);
+    }
+}
